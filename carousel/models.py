f24ebff9f1d7cfebe09bd8fb1216b6ad83305126
LiberBear <admin@liberbear.com>
2022-03-26 18:37:35 +0300

    template from bootstrap examples

---
+++ carousel/models.py
@@ -0,0 +1,26 @@
+from tabnanny import verbose
+from django.db import models
+
+# Create your models here.
+
+class CarouselPost(models.Model):
+    title = models.CharField(
+        verbose_name="Заголовок",
+        max_length=32
+    )
+
+    desc = models.CharField(
+        verbose_name="Описание",
+        max_length=64
+    )
+
+    img = models.ImageField(
+        verbose_name="Изображение",
+    )
+
+    class Meta:
+        verbose_name = "Запись в карусели"
+        verbose_name_plural = "Записи в карусели"
+    
+    def __str__(self) -> str:
+        return self.title
